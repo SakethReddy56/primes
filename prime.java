75292685305a842895dc9ffffceda2caa31e5d58
SakethReddy56 <sakethreddy28363@gmail.com>
2025-06-16 20:38:01 +0530

    Add files via upload

--- prime.java
+++ prime.java
@@ -1,6 +1,6 @@
 public class prime{
     public static void main(String[] args) {
-        System.out.println("prime numbers between 2 and 100:");
+        System.out.println("prime numBers between 2 and 100:");
         for(int num=2;num<=100;num++){
         boolean isPrime =true;
         for(int i=2;i<=Math.sqrt(num);i++){
